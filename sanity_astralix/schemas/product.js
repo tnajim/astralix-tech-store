185d5cb8196b21dc6c74c555e1c2f9daf2209cac
Taher Najim <taher.najim@yahoo.com>
2023-09-18 17:28:02 +0800

    created a sanity product schema for images and name

---
+++ sanity_astralix/schemas/product.js
@@ -0,0 +1,21 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{ type: 'image' }],
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+    ]
+}
