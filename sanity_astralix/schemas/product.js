769fbef85c8818083d7c807ba8f5f6216a20bb68
Taher Najim <taher.najim@yahoo.com>
2023-09-19 12:13:01 +0800

    added slug, price and details property to the product schema

--- sanity_astralix/schemas/product.js
+++ sanity_astralix/schemas/product.js
@@ -9,13 +9,32 @@ export default {
             type: 'array',
             of: [{ type: 'image' }],
             options: {
-                hotspot: true,
+                hotspot: true
             }
         },
         {
             name: 'name',
             title: 'Name',
-            type: 'string',
+            type: 'string'
         },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 90
+            }
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number'
+        },
+        {
+            name: 'details',
+            title: 'Details',
+            type: 'string'
+        }
     ]
 }
