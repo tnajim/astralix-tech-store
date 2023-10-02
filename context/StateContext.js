a50671d48cec102ef78912946f31b01995f704c3
Taher Najim <taher.najim@yahoo.com>
2023-10-02 13:00:24 +0800

    made the quantity options functional

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,9 +10,22 @@ export const StateContext = ({ children }) => {
     const [totalQuantities, setTotalQuantities] = useState();
     const [qty, setQty] = useState(1);
 
+    const incQty = () => {
+        setQty((prevQty) => prevQty + 1);
+    }
+
+    const decQty = () => {
+        setQty((prevQty) => {
+            if (prevQty - 1 < 1) return 1;
+            return prevQty - 1;
+        })
+    }
+
     return (
-        <Context.Provider value={{ showCart, cartItems, totalPrice, totalQuantities, qty }}>
+        <Context.Provider value={{ showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty }}>
             {children}
         </Context.Provider>
     )
 }
+
+export const useStateContext = () => useContext(Context);
