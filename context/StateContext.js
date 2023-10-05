c12cdd9a716214ce8cdb36e6e795d2093eab3049
Taher Najim <taher.najim@yahoo.com>
2023-10-05 14:26:40 +0800

    added onAdd function, checks if product exists in cart, otherwise: add

--- context/StateContext.js
+++ context/StateContext.js
@@ -5,10 +5,30 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
+
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find((item) => item._id === product._id);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + (product.price * quantity))
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+
+        if (checkProductInCart) {
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if (cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+            setCartItems(updatedCartItems);
+        } else {
+            product.quantity = quantity;
+            setCartItems([...cartItems, { ...product }])
+        }
+        toast.success(`${qty} ${product.name} added to the cart.`)
+    }
 
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
@@ -22,7 +42,17 @@ export const StateContext = ({ children }) => {
     }
 
     return (
-        <Context.Provider value={{ showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty }}>
+        <Context.Provider value={{
+            showCart,
+            setShowCart,
+            cartItems,
+            totalPrice,
+            totalQuantities,
+            qty,
+            incQty,
+            decQty,
+            onAdd
+        }}>
             {children}
         </Context.Provider>
     )
