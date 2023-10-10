ed604e06027071ad049f75001f3a61ac4934cc8f
Taher Najim <taher.najim@yahoo.com>
2023-10-10 16:59:25 +0800

    connected stripe to cart component (still needs config)

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -7,11 +7,29 @@ import toast from "react-hot-toast";
 
 import { useStateContext } from "@/context/StateContext";
 import { urlFor } from "@/lib/client";
+import getStripe from "@/lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
   const cartContainerRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+
+    const data = await response.json();
+    toast.loading('Redirecting...');
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
 
   // closes cart when clicked off
   useEffect(() => {
@@ -79,7 +97,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">
+              <button type="button" className="btn" onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
