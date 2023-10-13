5cbc8782b2e092a8b243f84b7fa81253137e5f57
Taher Najim <taher.najim@yahoo.com>
2023-10-13 17:21:08 +0800

    added "buy now" button functionality

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -8,7 +8,13 @@ import { useStateContext } from "@/context/StateContext";
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext(); // get react usestate functions from StateContext
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext(); // get react usestate functions from StateContext
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  }
 
   return (
     <div>
@@ -49,7 +55,7 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <div className="buttons">
             <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
-            <button type="button" className="buy-now">Buy Now</button>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
           </div>
         </div>
       </div>
