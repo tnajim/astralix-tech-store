fc828335fdea2fdd464ee258b530cb59b26223d3
Taher Najim <taher.najim@yahoo.com>
2023-10-16 17:04:09 +0800

    cleaned up console.log during stripe payment

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -5,7 +5,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
 
     if (req.method === 'POST') {
-        console.log(req.body);
         try {
             const params = {
                 submit_type: 'pay',
