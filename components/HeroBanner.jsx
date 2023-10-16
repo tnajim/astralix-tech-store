1948444f2755e7fe843ebeb3ee969e7f1c1848db
Taher Najim <taher.najim@yahoo.com>
2023-10-16 16:03:09 +0800

    cleanup some imports

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import Link from 'next/link';
-import { urlFor } from '../lib/client';
+import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
 
 const HeroBanner = ({ heroBanner }) => {
   return (
