a9fad4a4d3a9f28015fcb241475267283616f91f
Taher Najim <taher.najim@yahoo.com>
2023-10-18 13:14:26 +0800

    updated security policy to fix some console errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'font-src https://fonts.gstatic.com https://js.stripe.com https://m.stripe.network'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
